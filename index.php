0f36bfd1e307df99f9c0a744e76278e748f6ce6a
rangambr <rangambr@gmail.com>
2012-03-08 12:32:36 +0000

    changed greeting

--- index.php
+++ index.php
@@ -247,7 +247,7 @@ $app_name = idx($app_info, 'name', '');
     </header>
 
     <section id="get-started">
-      <p>Welcome to your Facebook app, running on <span>heroku</span>!</p>
+      <p>This is my app I'm editing it<span>heroku</span>!</p>
       <a href="https://devcenter.heroku.com/articles/facebook" target="_top" class="button">Learn How to Edit This App</a>
     </section>
 
